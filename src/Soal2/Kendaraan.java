fc909b9e206dd14edddac3b0a89f3f88cc848154
Bintang Khalik <bintangkhaliqq@gmail.com>
2025-04-29 13:10:34 +0700

    Nomor 2 Logic siap

---
+++ src/Soal2/Kendaraan.java
@@ -0,0 +1,46 @@
+package Soal2;
+
+public class Kendaraan {
+    String jenisKendaraan;
+    int lamaParkir;
+    double hargaParkir;
+
+    Kendaraan(String jenisKendaraan) {
+        this.jenisKendaraan = jenisKendaraan;
+    }
+
+    public double biayaParkir(int biaya, int jam) {
+        if (jam > 5) {
+            return biaya * jam * 0.9;
+        }
+        return biaya * jam;
+    }
+
+    public void hitungParkir(int jam) {
+        lamaParkir = jam;
+        hargaParkir =  switch (jenisKendaraan) {
+            case "Mobil" -> biayaParkir(5000, (jam));
+            case "Motor" -> biayaParkir(3000, (jam));
+            case "Truk" -> biayaParkir(8000, (jam));
+            default -> 0;
+        };
+    }
+
+    public void hitungParkir(int jamMasuk, int jamKeluar) {
+        lamaParkir = jamKeluar - jamMasuk;
+        hargaParkir =  switch (jenisKendaraan) {
+            case "Mobil" -> biayaParkir(5000, (jamKeluar - jamMasuk));
+            case "Motor" -> biayaParkir(3000, (jamKeluar - jamMasuk));
+            case "Truk" -> biayaParkir(8000, (jamKeluar - jamMasuk));
+            default -> 0;
+        };
+    }
+
+    public void Info() {
+        System.out.println("------ Bon Parkir ------");
+        System.out.println("Jenis kendaraan = " + jenisKendaraan);
+        System.out.println("Lama Parkir     = " + lamaParkir + " jam");
+        System.out.println("Total Biaya     = Rp." + hargaParkir);
+    }
+}
+
--- src/Soal2/Main.java
+++ src/Soal2/Main.java
@@ -1,4 +1,59 @@
 package Soal2;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char repeat = 'n';
+        int index = 0;
+        Kendaraan[] kendaraan = new Kendaraan[100];
+
+        System.out.println("|=====| Parking-Chan |=====|");
+        do {
+            System.out.print("\nMasukkan jenis kendaraan (Mobil/Motor/Truk) = ");
+            String jenis = sc.nextLine();
+            if (!(jenis.equals("Mobil") || jenis.equals("Motor") || jenis.equals("Truk"))) {
+                System.out.println("Kendaraan apa itu?");
+                break;
+            }
+            kendaraan[index] = new Kendaraan(jenis);
+
+            System.out.print("Masukkan durasi (Manual/Time) = ");
+            String pilihanDurasi = sc.nextLine();
+
+            switch (pilihanDurasi) {
+                case "Manual":
+                    System.out.print("Masukkan durasi (Jam) = ");
+                    kendaraan[index].hitungParkir(sc.nextInt());
+                    sc.nextLine();
+                    break;
+                case "Time":
+                    System.out.print("Masukkan jam masuk  = ");
+                    int jamIn = sc.nextInt();
+                    System.out.print("Masukkan jam keluar = ");
+                    int jamOut = sc.nextInt();
+                    sc.nextLine();
+                    kendaraan[index].hitungParkir(jamIn, jamOut);
+                    break;
+                default:
+                    System.out.println("Pilihan invalid");
+                    break;
+            }
+            kendaraan[index++].Info();
+            System.out.print("Apakah ada kendaraan lain? (y/n) = ");
+            repeat = sc.nextLine().charAt(0);
+        } while (repeat == 'y' || repeat == 'Y');
+
+        double totalBiaya = 0;
+        for (Kendaraan i : kendaraan) {
+            if (i != null)
+                totalBiaya += i.hargaParkir;
+        }
+        System.out.println("======= TOTAL =======");
+        System.out.println("Total Kendaraan = " + index);
+        System.out.println("Total Biaya     = " + totalBiaya);
+
+        sc.close();
+    }
 }
